d1f0212d2e5a88c2b3f83aea91f29a5e205ea8e5
sumon-hossain <sumonengr.96@gmail.com>
2023-05-19 18:31:38 +0600

    Scroll Triggered

--- src/components/FramerMotion.jsx
+++ src/components/FramerMotion.jsx
@@ -1,41 +1,45 @@
 /* eslint-disable react/prop-types */
-import { motion, useScroll } from "framer-motion"
+import { motion } from "framer-motion"
 
 const FramerMotion = () => {
-    const { scrollYProgress } = useScroll()
+
     return (
         <>
-            <motion.div style={{
-                scaleX: scrollYProgress,
-                position: "fixed",
-                top: 0,
-                right: 0,
-                left: 0,
-                height: 10,
-                background: "blue",
-                transformOrigin: "0%"
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center"
             }}
             >
 
-            </motion.div>
-            <h2>Scroll Demo</h2><br /><br />
-            <p>Lorem ipsum dolor sit amet consectetur</p><br /><br />
-            <p>Lorem ipsum dolor sit amet consectetur</p><br /><br />
-            <p>Lorem ipsum dolor sit amet consectetur</p><br /><br />
-            <p>Lorem ipsum dolor sit amet consectetur</p><br /><br />
-            <p>Lorem ipsum dolor sit amet consectetur</p><br /><br />
-            <p>Lorem ipsum dolor sit amet consectetur</p><br /><br />
-            <p>Lorem ipsum dolor sit amet consectetur</p><br /><br />
-            <p>Lorem ipsum dolor sit amet consectetur</p><br /><br />
-            <p>Lorem ipsum dolor sit amet consectetur</p><br /><br />
-            <p>Lorem ipsum dolor sit amet consectetur</p><br /><br />
-            <p>Lorem ipsum dolor sit amet consectetur</p><br /><br />
-            <p>Lorem ipsum dolor sit amet consectetur</p><br /><br />
-            <p>Lorem ipsum dolor sit amet consectetur</p><br /><br />
-            <p>Lorem ipsum dolor sit amet consectetur</p><br /><br />
-
-
+                <h2>Scroll Demo</h2>
+                <Box />
+                <Box />
+                <Box />
+                <Box />
+                <Box />
+                <Box />
+            </div>
         </>
+    )
+}
+
+const Box = () => {
+    return (
+        <motion.div
+            style={{
+                margin: "200px 0px",
+                width: "200px",
+                height: "200px",
+                background: "blue"
+            }}
+            initial={{ opacity: 0, scale: 0, x: 500 }}
+            whileInView={{ opacity: 1, scale: 1, x: 0 }}
+            transition={{ duration: 0.6 }}
+        // viewport={{ once: true }}
+        >
+
+        </motion.div>
     )
 }
 
